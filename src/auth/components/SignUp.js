b6edbb2492392be4f1825a1d7398e5d5d805af69
mmarsden89 <mmarsden89@gmail.com>
2019-07-19 15:41:06 -0400

    Update sign-up form and add the custom app title
    - Update sign-up form to include the new username field
      - As well update to api sign-up call
    
    - Add app title to multiple locations
      - Title of page updated
      - Header updated

--- src/auth/components/SignUp.js
+++ src/auth/components/SignUp.js
@@ -9,6 +9,7 @@ class SignUp extends Component {
     super()
 
     this.state = {
+      username: '',
       email: '',
       password: '',
       passwordConfirmation: ''
@@ -37,12 +38,20 @@ class SignUp extends Component {
   }
 
   render () {
-    const { email, password, passwordConfirmation } = this.state
+    const { username, email, password, passwordConfirmation } = this.state
 
     return (
       <form className='auth-form' onSubmit={this.onSignUp}>
         <h3>Sign Up</h3>
-
+        <label htmlFor="username">Username</label>
+        <input
+          required
+          name="username"
+          value={username}
+          type="username"
+          placeholder="username"
+          onChange={this.handleChange}
+        />
         <label htmlFor="email">Email</label>
         <input
           required
