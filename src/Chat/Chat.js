b5e3ba10d632caa11de2f503bb5e8f3f82da1ab2
mmarsden89 <mmarsden89@gmail.com>
2019-07-25 22:08:33 -0400

    Reworke compartment of components

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 import './Chat.scss'
 
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
 class Chat extends Component {
   constructor (props) {
     super(props)
 
     this.state = {
+      users: []
     }
   }
+
+  async componentDidMount () {
+    const userResponse = await axios(`${apiUrl}/users`)
+    this.setState({ users: userResponse.data.users })
+  }
+
   render () {
     return (
       <div id="chat-window" className="chat-window">
--- src/home/Home.js
+++ src/home/Home.js
@@ -3,23 +3,59 @@ import { withRouter } from 'react-router-dom'
 import Users from '../users/components/Users'
 import Chat from '../Chat/Chat'
 import './Home.scss'
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 class Home extends Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      users: []
+      users: [],
+      userfield: ''
     }
   }
 
   async componentDidMount () {
+    const response = await axios(`${apiUrl}/users`)
+    this.setState({ users: response.data.users })
+  }
+
+  handleChange = event => {
+    const updatedField = event.target.value
+    this.setState({ userfield: updatedField })
+    console.log(this.state.userfield)
+    console.log(this.state.users)
   }
 
   render () {
+    const users = this.state.users.filter(user => {
+      user.username.includes(this.state.userfield)
+    }).map(user => (
+      <h1 key={user._id}>user.username</h1>
+    ))
     return (
       <div className='home-body'>
-        <h1>Welcomes</h1>
+        <div className='searchbar-div'>
+          <FontAwesomeIcon
+            className="search-icon"
+            icon={faSearch}
+          />
+          <input
+            className="find-user"
+            required type="text"
+            name="userfield"
+            placeholder="find a user"
+            onChange={this.handleChange}
+            maxLength="160"
+          />
+          {users}
+        </div>
+        <div>
+        </div>
         <Users/>
         <Chat/>
       </div>
